04634165eda661e0393dfac16b8071bd00801508
Briaulin Mendez <b.j.mendezsena@gmail.com>
2020-08-24 19:44:54 +0200

    Git ignore de los usuarios de la base de datos

--- servidor/Constantes/ConstantesDataBase/configDB.js
+++ servidor/Constantes/ConstantesDataBase/configDB.js
@@ -1,8 +1,8 @@
 module.exports = CONFIGDB = {
     connectionLimit: 10,
     host: 'localhost',
-    user: 'wolprayusr',//TODO
-    password: 'UsrWolpray',//TODO
+    user: 'root',//TODO
+    password: '',//TODO
     database: 'wolpraydb_v01',
     //debug: true
 }
