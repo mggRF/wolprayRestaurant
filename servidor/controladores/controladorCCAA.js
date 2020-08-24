880ef01ac84c2c2f61329484446892ff4e893701
Briaulin Mendez <b.j.mendezsena@gmail.com>
2020-08-24 18:00:04 +0200

    Renombrado los nombres de los controladores

--- servidor/controladores/controladorCCAA.js
+++ servidor/controladores/controladorCCAA.js
@@ -6,32 +6,27 @@ const ControladorBase = require("./ControladorBase");
 
 const MODELO = require("../modelos/Comunidades");
 const TABLA = 'c_state';
-const SELECT_UNO = `SELECT * FROM ${TABLA} WHERE stateid = :id`;
-const SELECT_SELECT = `SELECT stateid as id,stateName as opcion FROM ${TABLA} WHERE countryid = :id`
+
+const QUERIES = {
+    SELECT_UNO: `SELECT * FROM ${TABLA} WHERE stateid = :id`,
+    SELECT_SELECT: `SELECT stateid as id,stateName as opcion FROM ${TABLA} WHERE countryid = :id`,
+    INSERT: `INSERT INTO ${TABLA} SET ?`,
+    UPDATE: `UPDATE ${TABLA} SET ? WHERE stateid = ?`,
+    DELETE: `DELETE FROM ${TABLA} WHERE stateid = ?`
+}
+
 
 class ControladorCCAA extends ControladorBase {
    
     constructor(){
         let config = {
-            TABLA:TABLA,
-            SELECT_UNO : SELECT_UNO,
-            SELECT_SELECT: SELECT_SELECT,
-            MODELO:MODELO,
+            TABLA: TABLA,
+            QUERIES: QUERIES,
+            MODELO: MODELO,
             campoId: 'stateid',
         }
         super(config);
     }
-
-    hacerPut(){
-
-    }
-    
-    hacerPost(req,res){
-
-    }
-
-    
-
 
 }
 
