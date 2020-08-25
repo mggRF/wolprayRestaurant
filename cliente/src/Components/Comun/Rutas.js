925def19eea557647796594e14ee1e72114be386
migarcia <migarcia@recursosformacion.com>
2020-08-25 07:56:01 +0200

    Clientes1

--- cliente/src/Components/Comun/Rutas.js
+++ cliente/src/Components/Comun/Rutas.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 //import PropTypes from 'prop-types'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ControllerMusic from '../Controladores/music/ControllerMusic';
+import ControllerClub from '../Controladores/clubs/ControllerClub';
+import Home from '../Paginas/Home';
 
 
 
 
-export default class  Rutas extends Component {
-    render(){
+export default class Rutas extends Component {
+    render() {
         return (
-            <h1>Esto es rutas</h1>
-           
+            <>
+            <Link to={`${process.env.REACT_APP_PUBLIC_URL}/`}>inicio</Link>
+            <Link to={`${process.env.REACT_APP_PUBLIC_URL}/music`}>musica</Link>
+            <Link to={`${process.env.REACT_APP_PUBLIC_URL}/clubs`}>clubs</Link>
+            <Router basename={'/interno'}>
+                <Route path={`${REACT_APP_PUBLIC_URL}/`} component={Home} />
+                <Route path={`${process.env.REACT_APP_PUBLIC_URL}/music`} component={ControllerMusic} />
+                <Route path={`${process.env.REACT_APP_PUBLIC_URL}/clubs`} component={ControllerClub} />
+            </Router>
+
+           </>
         )
     }
 }
