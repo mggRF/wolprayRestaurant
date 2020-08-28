73a5ba7e6d653ce89ff287a10422ce4894e0daa6
Briaulin Mendez <b.j.mendezsena@gmail.com>
2020-08-28 18:03:21 +0200

    Actualizados los controladores del servidor y la db

--- servidor/controladores/ControladorClubs.js
+++ servidor/controladores/ControladorClubs.js
@@ -6,9 +6,32 @@ const ControladorBase = require("./ControladorBase");
 
 const MODELO = require("../modelos/Club");
 const TABLA = 'clubs';
+const selectUno  = `select  
+                        ${TABLA}.*, 
+                        n_dresscode.dressCodeDescription,
+                        companies.companyName,
+                        c_city.cityName,
+                        c_provinces.provinceName,
+                        c_state.stateName,
+                        c_country.countryName,
+                        GROUP_CONCAT(n_music.musicName) as Musica
+
+                        from ${TABLA} 
+
+                            LEFT JOIN club_music ON club_music.clubid = ${TABLA}.clubid
+                            LEFT JOIN n_music on club_music.musicid = n_music.musicid
+                            JOIN n_dresscode ON n_dresscode.dressCodeId = ${TABLA}.dressCodeid
+                            JOIN companies on companies.companyid = ${TABLA}.companyid
+                            JOIN c_city ON c_city.cityid = ${TABLA}.cityid
+                            JOIN c_provinces ON c_provinces.provinceid = c_city.provinceid
+                            JOIN c_state ON c_state.stateid = c_provinces.stateid
+                            JOIN c_country on c_country.countryId = c_state.countryid
+
+                        WHERE ${TABLA}.clubid = :id
+                        GROUP BY ${TABLA}.clubid;`;
 
 const QUERIES = {
-    SELECT_UNO: `SELECT * FROM ${TABLA} WHERE clubid  = :id`,
+    SELECT_UNO: selectUno,
     SELECT_SELECT: `SELECT clubid as id,clubName as opcion FROM ${TABLA} WHERE cityid = :id`,
     INSERT: `INSERT INTO ${TABLA} SET ?`,
     UPDATE: `UPDATE ${TABLA} SET ? WHERE clubid = ?`,
