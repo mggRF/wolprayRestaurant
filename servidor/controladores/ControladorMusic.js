256c7fd1caf65261d9bc9251868a29d889a458ca
migarcia <migarcia@recursosformacion.com>
2020-09-02 18:38:54 +0200

    aa

--- servidor/controladores/ControladorMusic.js
+++ servidor/controladores/ControladorMusic.js
@@ -22,7 +22,7 @@ const TABLA = 'n_music';
 
 const QUERIES = {
     SELECT_ALL: `SELECT * FROM ${TABLA} WHERE musicid = :ids`,
-    SELECT_SELECT: `SELECT musicid as id,musicName as opcion FROM ${TABLA} ,
+    SELECT_SELECT: `SELECT musicid as id,musicName as opcion FROM ${TABLA}` ,
     SELECT_UNO: `SELECT * FROM ${TABLA} WHERE musicid = :id`,
     INSERT: `INSERT INTO ${TABLA} SET ?`,
     UPDATE: `UPDATE ${TABLA} SET ? WHERE musicid = ?`,
