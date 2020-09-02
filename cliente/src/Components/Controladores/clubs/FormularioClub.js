0e1827c67af2b58f0ad7d4676eb5a7b94cfb6eb8
Jeulinser Mendez <jeulinsermendez@gmail.com>
2020-09-02 13:20:18 +0200

    formulario clubs

--- cliente/src/Components/Controladores/clubs/FormularioClub.js
+++ cliente/src/Components/Controladores/clubs/FormularioClub.js
@@ -1,56 +1,151 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Imagen from '../../Fragmentos/Imagen';
+import { InputComponent } from '../companies/InputComponent';
 
 export default class  FormularioClub extends Component {
     render() {
         let club = this.props.obj;
-        let readonly = this.props.orden.includes(['D', 'V']) ? true : false
-
-
-        if(this.props.orden === "V"){
-            return(
-                <div className="container homepage " >
-                <div className="container jumbotron text-center page-header">
-                <br/>
-                <Imagen imageUrl = {club.coverUrl}/>
-                <br/>
-                <h1>{club.clubName.toUpperCase()}</h1>
-                <br/>
-                <h6>{club.description.toUpperCase()}</h6>
-                <br/>
-                <br/>
-                </div>
-                   <div className = "row">
-                   <div className = "col-4 ">
-                   <div > <h5>ID: {club.clubid} </h5></div>
-                   <div> <h6>Dirección: </h6> </div>
-                   <div > <h6>Número de teléfono: </h6> </div>
-                   </div>
-                   <div className = "col-8 ">
-                   <div > <h5>DETALLES</h5></div> 
-                   <h6>{club.streetName.toUpperCase()}, {club.streetNumber}, {club.postal_code}</h6>
-                   <h6>{club.clubPhone}</h6>
-                   </div>
-                   </div>
-                </div>
-            )
-
-        }
+        console.log("Desde formularioClub", club)
+        let readonly = false
+        if (this.props.orden == 'D' || this.props.orden == 'V') readonly = true
         
         return (
             <div>
-                <label htmlFor="clubid">Id</label>
-                <input name="clubid" 
-                    value={club.clubid } 
-                    onChange={this.props.funcion} 
-                    readOnly="ON"/>
-                <label htmlFor="clubName">Name</label>
-                <input name="clubName" 
-                    value={club.clubName} 
-                    onChange={this.props.funcion} 
-                    readOnly={readonly} />
+                
+                <InputComponent name="clubid" 
+                    handleChange={this.props.funcion}
+                    name="clubid"
+                    label="ID"
+                    readOnly={readonly}
+                    value={club.clubid}/>
+                
+                <InputComponent name="clubName" 
+                    handleChange={this.props.funcion}
+                    name="clubName"
+                    label="Nombre"
+                    readOnly={readonly}
+                    value={club.clubName}/>
+
+                <InputComponent name="description" 
+                    handleChange={this.props.funcion}
+                    name="description"
+                    label="Descripción"
+                    readOnly={readonly}
+                    value={club.description}/>
+
+                <InputComponent name="Musica" 
+                    handleChange={this.props.funcion}
+                    name="Musica"
+                    label="Musica"
+                    readOnly={readonly}
+                    value={club.Musica}/>
+
+                
+                <InputComponent name="clubPhone" 
+                    handleChange={this.props.funcion}
+                    name="clubPhone"
+                    label="Teléfono"
+                    readOnly={readonly}
+                    value={club.clubPhone}/>
+
+                <InputComponent name="dressCodeDescription" 
+                    handleChange={this.props.funcion}
+                    name="dressCodeDescription"
+                    label="Código de vestimenta"
+                    readOnly={readonly}
+                    value={club.dressCodeDescription}/>
+                
+                <InputComponent name="companyName" 
+                    handleChange={this.props.funcion}
+                    name="companyName"
+                    label="Empresa"
+                    readOnly={readonly}
+                    value={club.companyName}/>
+
+                
+
+                <InputComponent name="postal_code" 
+                    handleChange={this.props.funcion}
+                    name="postal_code"
+                    label="Codigo postal"
+                    readOnly={readonly}
+                    value={club.postal_code}/>
+                <InputComponent name="stateName" 
+                    handleChange={this.props.funcion}
+                    name="stateName"
+                    label="Estado / Comunidad autónoma"
+                    readOnly={readonly}
+                    value={club.stateName}/>
+
+                <InputComponent name="provinceName" 
+                    handleChange={this.props.funcion}
+                    name="provinceName"
+                    label="Provincia"
+                    readOnly={readonly}
+                    value={club.provinceName}/>
+                    
+                <InputComponent name="cityName" 
+                    handleChange={this.props.funcion}
+                    name="cityName"
+                    label="Ciudad"
+                    readOnly={readonly}
+                    value={club.cityName}/>
+                
+
+                <InputComponent name="streetName" 
+                    handleChange={this.props.funcion}
+                    name="streetName"
+                    label="Nombre de la calle"
+                    readOnly={readonly}
+                    value={club.streetName}/>
+                <InputComponent name="streetNumber" 
+                    handleChange={this.props.funcion}
+                    name="streetNumber"
+                    label="Número de la calle"
+                    readOnly={readonly}
+                    value={club.streetNumber}/>
+                <InputComponent name="accessAge" 
+                    handleChange={this.props.funcion}
+                    name="accessAge"
+                    label="Edad mínima"
+                    readOnly={readonly}
+                    value={club.accessAge}/>
+                <InputComponent name="DiasAnticipacion" 
+                    handleChange={this.props.funcion}
+                    name="DiasAnticipacion"
+                    label="Dias Anticipacion"
+                    readOnly={readonly}
+                    value={club.DiasAnticipacion}/>
+                <InputComponent name="DiasAnticipacion" 
+                    handleChange={this.props.funcion}
+                    name="DiasAnticipacion"
+                    label="Dias Anticipacion"
+                    readOnly={readonly}
+                    value={club.DiasAnticipacion}/>
+                <InputComponent name="entryCost" 
+                    handleChange={this.props.funcion}
+                    name="entryCost"
+                    label="Coste de entrada"
+                    readOnly={readonly}
+                    value={club.entryCost}/>
+
+                <InputComponent name="maxPeople" 
+                    handleChange={this.props.funcion}
+                    name="maxPeople"
+                    label="Máximo de personas"
+                    readOnly={readonly}
+                    value={club.maxPeople}/>
             </div>
+            /*
+
+
+coverUrl: null
+howToGetThere: null
+latitude: null
+longitude: null
+
+            */ 
 
 
         )
