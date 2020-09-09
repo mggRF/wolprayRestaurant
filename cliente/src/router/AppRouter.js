66356d770514e9d1d7d1334cf2bc0b62d7a6c1d8
Briaulin Mendez <b.j.mendezsena@gmail.com>
2020-09-09 09:12:11 +0200

    Actualización: Limpieza de errores de lewis

--- cliente/src/router/AppRouter.js
+++ cliente/src/router/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Rutas from '../Components/Comun/Rutas'
 import LoginScreen  from '../Components/LoginComponent/LoginScreen'
+import RegisterComponent from '../Components/RegisterComponent/RegisterComponent'
 
 export const AppRouter = () => {
     return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
             <div>
             <Switch>
                 <Route exact path ='/login' component ={LoginScreen}/>
+                <Route exact path ='/register' component ={RegisterComponent}/>
                 <Route path ='/' component ={Rutas}/>
             </Switch>
             </div>
