af93102a77686c3e38e8cd18269d9fd9062a70f6
migarcia <migarcia@recursosformacion.com>
2020-09-10 18:35:11 +0200

    correciones login

--- cliente/src/Servicios/funcionesSeguridad.js
+++ cliente/src/Servicios/funcionesSeguridad.js
@@ -1,14 +1,14 @@
 export function checkUsuario(role) {
-
+    let init = JSON.parse(localStorage.getItem('user')) || { logged: false };
     if (role >= init.role) {
-
         return {
             id: init.id,
             role: init.role,
             token: init.token
         };
-    }else{
-
+    } else {
+        //******************    window.history.back();    //solucion 1
+        window.location.href = '/login/';
     }
 }
 
