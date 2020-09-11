c493bafd548710182948f79474a73a7ccdbbeb4e
Jeulinser Mendez <jeulinsermendez@gmail.com>
2020-09-11 12:10:46 +0200

    controlador slot

--- servidor/controladores/ControladorSlot.js
+++ servidor/controladores/ControladorSlot.js
@@ -10,6 +10,7 @@ const TABLA = 'slots';
 const QUERIES = {
     SELECT_SELECT: `SELECT slotid  as id,clubid  as opcion FROM ${TABLA} WHERE clubid  = :id`,
     SELECT_UNO: `SELECT * FROM ${TABLA} WHERE slotid  = :id`,
+    SELECT_MES_CLUB: `SELECT * FROM ${TABLA} WHERE clubid  = :id AND `,
     INSERT: `INSERT INTO ${TABLA} SET ?`,
     UPDATE: `UPDATE ${TABLA} SET ? WHERE slotid = ?`,
     DELETE: `DELETE FROM ${TABLA} WHERE slotid = ?`
