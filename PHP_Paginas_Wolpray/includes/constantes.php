6a98e1fe73ac7324c28944d7a7ad5da275dd8c28
migarcia <migarcia@recursosformacion.com>
2020-09-19 16:43:58 +0200

    ajustes api

--- PHP_Paginas_Wolpray/includes/constantes.php
+++ PHP_Paginas_Wolpray/includes/constantes.php
@@ -4,7 +4,7 @@
  * ***** Area de control ******************* 
  */
 define('API_BASE_DEVELOPPER', "http://localhost:3800/");   //direccion base
-define('API_BASE_PRODUCTION', "http://wolpray.es:3800/");   //direccion base
+define('API_BASE_PRODUCTION', "http://api.wolpray.es:3800/");   //direccion base
 
 define('VERSION_API', "api_v00");
 //define('API_URL', API_BASE_DEVELOPPER . VERSION_API . '/');
