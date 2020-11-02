b0b09fe9c881424f3f42ee9684e5cb53f1b02d25
migarcia <migarcia@recursosformacion.com>
2020-11-02 17:57:32 +0100

    inicial-3

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -17,7 +17,7 @@ add_action( 'init', function() {
           wp_enqueue_style( 'erw', get_site_url() . $asset_manifest[ 'main.css' ] );
         }
     
-        wp_enqueue_script( 'erw-runtime', get_site_url() . $asset_manifest[ 'runtime~main.js' ], array(), null, true );
+        wp_enqueue_script( 'erw-runtime', get_site_url() . $asset_manifest[ 'runtime-main.js' ], array(), null, true );
     
         wp_enqueue_script( 'erw-main', get_site_url() . $asset_manifest[ 'main.js' ], array('erw-runtime'), null, true );
     
